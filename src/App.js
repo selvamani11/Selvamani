dfa8855770a9d0b7de9f9df00c402bf1c9e035c1
Selvamani Kasiraja <selvamani.kasiraja@sandhata.com>
2023-12-01 15:18:23 +0530

    meal manager react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MealManager from './component/MealManager';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>The Meal Manager</h1>
+      <MealManager />
+    </div>
+  );
+}
+
+export default App;
